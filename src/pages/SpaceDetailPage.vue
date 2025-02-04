836a2d7dff9a03fdb46a331005515837008e0b5d
阳光阿盖尔 <wj52030000@outlook.com>
2025-02-04 21:40:38 +0800

    图片批量处理功能

--- src/pages/SpaceDetailPage.vue
+++ src/pages/SpaceDetailPage.vue
@@ -7,6 +7,7 @@
         <a-button type="primary" :href="`/add_picture?spaceId=${id}`" target="_blank">
           + 创建图片
         </a-button>
+        <a-button :icon="h(EditOutlined)" @click="doBatchEdit"> 批量编辑</a-button>
         <a-tooltip
           :title="`占用图片 ${formatSize(space.totalSize)} / ${formatSize(space.maxSize)}`"
         >
@@ -37,12 +38,19 @@
       :show-total="() => `图片总数 ${total} / ${space.maxCount}`"
       @change="onPageChange"
     />
+    <BatchEditPictureModal
+      ref="batchEditPictureModalRef"
+      :spaceId="id"
+      :pictureList="dataList"
+      :onSuccess="onBatchEditPictureSuccess"
+    />
+
   </div>
 </template>
 
 <script setup lang="ts">
 // 数据
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, reactive, ref, h} from 'vue'
 import { message } from 'ant-design-vue'
 import { getSpaceVoByIdUsingGet } from '@/api/spaceController.ts'
 import { formatSize } from '@/utils'
@@ -51,6 +59,8 @@ import PictureList from '@/components/PictureList.vue'
 import PictureSearchForm from '@/components/PictureSearchForm.vue'
 import { ColorPicker } from 'vue3-colorpicker'
 import 'vue3-colorpicker/style.css'
+import BatchEditPictureModal from '@/components/BatchEditPictureModal.vue'
+import { EditOutlined } from '@ant-design/icons-vue'
 
 const props = defineProps<{
   id: string | number
@@ -145,6 +155,21 @@ const onColorChange = async (color: string) => {
   }
   loading.value = false
 }
+
+// 分享弹窗引用
+const batchEditPictureModalRef = ref()
+
+// 批量编辑成功后，刷新数据
+const onBatchEditPictureSuccess = () => {
+  fetchData()
+}
+// 打开批量编辑弹窗
+const doBatchEdit = () => {
+  if (batchEditPictureModalRef.value) {
+    batchEditPictureModalRef.value.openModal()
+  }
+}
+
 
 </script>
 
