af46b873695824ab2396220c588286c92d727831
阳光阿盖尔 <wj52030000@outlook.com>
2025-02-09 22:36:06 +0800

    空间管理补充空间类别

--- src/pages/admin/SpaceManagePage.vue
+++ src/pages/admin/SpaceManagePage.vue
@@ -5,9 +5,6 @@
       <h2>空间管理</h2>
       <a-space>
         <a-button type="primary" href="/add_space" target="_blank">+ 创建空间</a-button>
-      </a-space>
-      <a-space>
-        <a-button type="primary" href="/add_space" target="_blank">+ 创建空间</a-button>
         <a-button type="primary" ghost href="/space_analyze?queryPublic=1" target="_blank">
           分析公共图库
         </a-button>
@@ -33,6 +30,16 @@
           allow-clear
         />
       </a-form-item>
+      <a-form-item label="空间类别" name="spaceType">
+        <a-select
+          v-model:value="searchParams.spaceType"
+          :options="SPACE_TYPE_OPTIONS"
+          placeholder="请输入空间类别"
+          style="min-width: 180px"
+          allow-clear
+        />
+      </a-form-item>
+
       <a-form-item label="用户 id" name="userId">
         <a-input v-model:value="searchParams.userId" placeholder="请输入用户 id" allow-clear />
       </a-form-item>
@@ -56,6 +63,11 @@
         <template v-if="column.dataIndex === 'spaceLevel'">
           <a-tag>{{ SPACE_LEVEL_MAP[record.spaceLevel] }}</a-tag>
         </template>
+        <!-- 空间类别 -->
+        <template v-if="column.dataIndex === 'spaceType'">
+          <a-tag>{{ SPACE_TYPE_MAP[record.spaceType] }}</a-tag>
+        </template>
+
         <!-- 使用情况 -->
         <template v-if="column.dataIndex === 'spaceUseInfo'">
           <div>大小：{{ formatSize(record.totalSize) }} / {{ formatSize(record.maxSize) }}</div>
@@ -91,7 +103,7 @@ import { formatSize } from '@/utils'
 import { message } from 'ant-design-vue'
 import dayjs from 'dayjs'
 import { deleteSpaceUsingPost, listSpaceByPageUsingPost } from '@/api/spaceController.ts'
-import { SPACE_LEVEL_OPTIONS ,SPACE_LEVEL_MAP} from '@/constants/space.ts'
+import { SPACE_LEVEL_OPTIONS, SPACE_LEVEL_MAP, SPACE_TYPE_MAP, SPACE_TYPE_OPTIONS } from '@/constants/space.ts'
 
 
 const columns = [
@@ -108,6 +120,11 @@ const columns = [
     title: '空间级别',
     dataIndex: 'spaceLevel',
   },
+  {
+    title: '空间类别',
+    dataIndex: 'spaceType',
+  },
+
   {
     title: '使用情况',
     dataIndex: 'spaceUseInfo',
